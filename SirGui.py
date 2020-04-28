cdfdc02386405f6a881f5897bd5f95fbced56f23
ed <ed@cordwells.com>
2020-04-27 23:47:34 -0400

    Version 1.0

---
+++ SirGui.py
@@ -0,0 +1,391 @@
+import sys
+import matplotlib
+import numpy as np
+from PyQt5.QtWidgets import qApp
+
+from SirModel import SirModel, Cols
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas, NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+matplotlib.use('Qt5Agg')
+
+
+def make_format(current, other):
+    # current and other are axes
+    def format_coord(x, y):
+        # x, y are data coordinates
+        # convert to display coords
+        display_coord = current.transData.transform((x,y))
+        inv = other.transData.inverted()
+        # convert back to data coords with respect to ax
+        ax_coord = inv.transform(display_coord)
+        coords = [ax_coord, (x, y)]
+        return ('Left: {:<40}    Right: {:<}'
+                .format(*['({:.3f}, {:.3f})'.format(x, y) for x,y in coords]))
+    return format_coord
+
+
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.resize(900, 660)
+        self.cntrls = []
+        self.newPlot = True
+
+        self.centralwidget = QtWidgets.QWidget(self)
+        self.setCentralWidget(self.centralwidget)
+        self.setWindowTitle("SIR Model")
+
+        self.menubar = QtWidgets.QMenuBar(self)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setTitle("File")
+        self.setMenuBar(self.menubar)
+        self.actionOpen = QtWidgets.QAction(self)
+        self.actionOpen.setText("Open")
+        self.actionSave = QtWidgets.QAction(self)
+        self.actionSave.setText("Save")
+        self.actionQuit = QtWidgets.QAction(self)
+        self.actionQuit.setText("Quit")
+        self.actionQuit.setShortcut('Ctrl+Q')
+
+        self.menuFile.addAction(self.actionOpen)
+        self.menuFile.addAction(self.actionSave)
+        self.menuFile.addSeparator()
+        self.menuFile.addAction(self.actionQuit)
+        self.menubar.addAction(self.menuFile.menuAction())
+
+        self.actionSave.triggered.connect(self.SaveFile)
+        self.actionOpen.triggered.connect(self.OpenFile)
+        self.actionQuit.triggered.connect(qApp.quit)
+
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setContentsMargins(2, 2, 2, 2)
+        self.horizontalLayout.setSpacing(3)
+
+        self.CtrlsStatsLayout = QtWidgets.QVBoxLayout()
+        self.CtrlsStatsLayout.setContentsMargins(-1, -1, 0, -1)
+        self.horizontalLayout.addLayout(self.CtrlsStatsLayout)
+
+        self.splitter = QtWidgets.QSplitter(self.centralwidget)
+        self.splitter.setFrameShape(QtWidgets.QFrame.Box)
+        self.splitter.setChildrenCollapsible(False)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        self.splitter.setSizePolicy(sizePolicy)
+        self.splitter.setOrientation(QtCore.Qt.Vertical)
+        self.horizontalLayout.addWidget(self.splitter)
+
+        self.SirParamsBox = QtWidgets.QGroupBox(self.centralwidget)
+        self.SirParamsBox.setTitle("SIR Controls")
+        self.horizontalLayout_6 = QtWidgets.QHBoxLayout(self.SirParamsBox)
+        self.horizontalLayout_6.setContentsMargins(2, 6, 2, 2)
+        self.horizontalLayout_6.setSpacing(2)
+        self.CtrlsStatsLayout.addWidget(self.SirParamsBox)
+
+        self.SPLabelLayout = QtWidgets.QVBoxLayout()
+        self.SPLabelLayout.setContentsMargins(-1, 5, 2, -1)
+        self.SPLabelLayout.setSpacing(7)
+        self.horizontalLayout_6.addLayout(self.SPLabelLayout)
+
+        self.SPeditsLayout = QtWidgets.QVBoxLayout()
+        self.SPeditsLayout.setContentsMargins(-1, -1, 0, -1)
+        self.SPeditsLayout.setSpacing(0)
+        self.horizontalLayout_6.addLayout(self.SPeditsLayout)
+
+        self.AddParameters()
+
+        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.SPLabelLayout.addItem(spacerItem)
+
+        spacerItem1 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.SPeditsLayout.addItem(spacerItem1)
+
+        self.SirStatsBox = QtWidgets.QGroupBox(self.centralwidget)
+        self.SirStatsBox.setTitle("SIR Stats")
+        self.horizontalLayout_5 = QtWidgets.QHBoxLayout(self.SirStatsBox)
+        self.horizontalLayout_5.setContentsMargins(0, 0, 0, 4)
+        self.CtrlsStatsLayout.addWidget(self.SirStatsBox)
+
+        self.statsNames = QtWidgets.QLabel(self.SirStatsBox)
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.statsNames.setFont(font)
+
+        self.statsNames.setText("Day\n"
+                                "n Susceptable\n"
+                                "n Infected\n"
+                                "n Recovered\n"
+                                "n Dead\n"
+                                "New Infected\n"
+                                "New Recover/Died\n"
+                                "Infection Rate")
+        self.statsNames.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTop | QtCore.Qt.AlignTrailing)
+        self.statsNames.setIndent(0)
+        self.horizontalLayout_5.addWidget(self.statsNames)
+
+        self.StatsData = QtWidgets.QLabel(self.SirStatsBox)
+        # font = QtGui.QFont()
+        # font.setPointSize(10)
+        self.StatsData.setFont(font)
+        self.StatsData.setText("1\n10000\n0\n\0\n")
+        self.StatsData.setAlignment(QtCore.Qt.AlignLeading | QtCore.Qt.AlignLeft | QtCore.Qt.AlignTop)
+        self.horizontalLayout_5.addWidget(self.StatsData)
+
+        self.ButtonLayout = QtWidgets.QHBoxLayout()
+        self.ButtonLayout.setContentsMargins(-1, 0, -1, -1)
+
+        self.ResetButton = QtWidgets.QPushButton(self.centralwidget)
+        self.ResetButton.setMaximumSize(QtCore.QSize(50, 16777215))
+        self.ResetButton.setText("Reset")
+        self.ResetButton.clicked.connect(self.ResetClicked)
+        self.ButtonLayout.addWidget(self.ResetButton)
+
+        self.NewPlot = QtWidgets.QPushButton(self.centralwidget)
+        self.NewPlot.setMaximumSize(QtCore.QSize(50, 16777215))
+        self.NewPlot.setText("NewPlot")
+        self.NewPlot.clicked.connect(self.NewPlotClicked)
+        self.ButtonLayout.addWidget(self.NewPlot)
+
+        self.RunButton = QtWidgets.QPushButton(self.centralwidget)
+        self.RunButton.setMaximumSize(QtCore.QSize(50, 16777215))
+        self.RunButton.setText("Run")
+        self.RunButton.clicked.connect(self.RunClicked)
+        self.ButtonLayout.addWidget(self.RunButton)
+
+        self.CtrlsStatsLayout.addLayout(self.ButtonLayout)
+        self.horizontalLayout.addLayout(self.CtrlsStatsLayout)
+
+        # add the plots
+        self.TopPlotFrame = QtWidgets.QFrame(self.splitter)
+        self.TopPlotFrame.setFrameShape(QtWidgets.QFrame.Box)
+        self.TopPlotFrame.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.TopPlotFrame)
+
+        # scatter plot of people with color = status
+        self.TPfigure = Figure()
+        self.tpAx = self.TPfigure.add_subplot(111)
+        self.Topcanvas = FigureCanvas(self.TPfigure)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Preferred)
+        self.Topcanvas.setSizePolicy(sizePolicy)
+        toolbar = NavigationToolbar(self.Topcanvas, self)
+
+        self.verticalLayout.addWidget(toolbar)
+        self.verticalLayout.addWidget(self.Topcanvas)
+
+        x, y, s, c = np.random.rand(4, 20000)
+        self.tpAx.scatter(x, y, 1, marker='.')
+
+        # line plot of number of people vs day
+        self.BottomPlotFrame = QtWidgets.QFrame(self.splitter)
+        self.BottomPlotFrame.setFrameShape(QtWidgets.QFrame.Box)
+        self.BottomPlotFrame.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.BottomPlotFrame)
+
+        self.PvDfigure = Figure()  # PvD = PeepsVsDay
+        self.PvDax = self.PvDfigure.add_subplot(111)
+        self.ax2 = self.PvDax.twinx()
+        self.ax2.format_coord = make_format(self.ax2, self.PvDax)
+
+        self.PvDcanvas = FigureCanvas(self.PvDfigure)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        self.PvDcanvas.setSizePolicy(sizePolicy)
+        toolbar2 = NavigationToolbar(self.PvDcanvas, self)
+
+        self.verticalLayout_2.addWidget(toolbar2)
+        self.verticalLayout_2.addWidget(self.PvDcanvas)
+
+        x = np.linspace(0, 10)
+        self.PvDax.plot(x, np.sin(x) + x + np.random.randn(50))
+        self.PvDax.plot(x, np.sin(x) + 0.5 * x + np.random.randn(50))
+        self.PvDax.plot(x, np.sin(x) + 2 * x + np.random.randn(50))
+        self.PvDax.plot(x, np.sin(x) - 0.5 * x + np.random.randn(50))
+        self.PvDax.plot(x, np.sin(x) - 2 * x + np.random.randn(50))
+        self.PvDax.plot(x, np.sin(x) + np.random.randn(50))
+        self.PvDax.set_title("filler plot")
+
+        self.playTimer = QtCore.QTimer()
+        self.playTimer.setInterval(50)
+        self.playTimer.timeout.connect(self.Run)
+
+        self.running = False
+        self.day = 0
+        self.SirModel = SirModel(self.cntrls)
+
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+    def RunClicked(self):
+        if self.running:
+            self.running = False
+            self.playTimer.stop()
+            self.RunButton.setText('Run')
+        else:
+            self.running = True
+            self.playTimer.start()
+            self.RunButton.setText('Pause')
+
+    def SaveFile(self):
+        pass
+
+    def OpenFile(self):
+        pass
+
+    def ResetClicked(self):
+        self.day = 0
+        self.SirModel.Reset()
+
+    def NewPlotClicked(self):
+        self.newPlot = True
+        self.PvDax.cla()
+
+        # self.running = False
+        # self.playTimer.stop()
+
+    def Run(self):
+        if not self.running:
+            return
+
+        self.DrawStuff()
+
+        self.day += 1
+        self.SirModel.InfectedToRecovered(self.day)
+        self.SirModel.SpreadInfection(self.day)
+
+    def DrawStuff(self):
+        self.TopPlot()
+
+    def AddParameters(self):
+        # param data must be in same order as SirModel.Ctrls
+        paramData = [['nPeeps', 5000, 'number of people in the universe'],
+                  ['min Days Sick', 10, 'days a guy will be sick before he gets better or dies'],
+                  ['pRecover', 0.54, 'Prob that a guys will recover on a given day. Applied after min days sick'],
+                  ['pDie', 0.04,'Prob that a guys will die on a given day. Applied after min days sick'],
+                  ['nOwn Gp', 3, 'Number of guys in your own group you see every day'],
+                  ['nStranger', 5, 'Number of strangers you see each day. Draw from strange universe'],
+                  ['Strgr Univ', 20, 'The universe of guys you could possibly see. Local Univ = [your Id +- strgr univ'],
+                  ['pService Guy', 0.04, 'Prob that any given guy is in a service industry'],
+                  ['Serv nStranger', 10,''],
+                  ['Serv StrgrUniv', 30,''],
+                  ['pInfect own', 0.2,''],
+                  ['pInfect Stgr', 0.05,''],
+                  ['pContTrace', 0.04,''],
+                  ['nContTrace/day', 0.04,''],
+                  ['pObey SIP', 0.9,'']]
+
+        for n, param in enumerate(paramData):
+            pName = param[0]
+            pVal = param[1]
+            toolTip = param[2]
+            label = QtWidgets.QLabel(pName, self.SirParamsBox)
+            sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Preferred)
+            label.setSizePolicy(sizePolicy)
+            label.setToolTip(toolTip)
+
+            label.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
+            self.SPLabelLayout.addWidget(label, 0, QtCore.Qt.AlignRight)
+
+            lineEdit = QtWidgets.QLineEdit(self.SirParamsBox)
+            sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Preferred)
+            lineEdit.setSizePolicy(sizePolicy)
+            lineEdit.setFixedWidth(80)
+            lineEdit.setText(str(pVal))
+            self.cntrls.append(lineEdit)
+            self.SPeditsLayout.addWidget(self.cntrls[n])
+
+    def TopPlot(self):
+        x = self.SirModel.data[:, Cols.xPos]
+        y = self.SirModel.data[:, Cols.yPos]
+
+        # nonservice susceptables
+        nsp = np.logical_and(self.SirModel.data[:, Cols.status] == 0, (self.SirModel.data[:, Cols.serviceGuy] == 0))
+
+        # service susceptables
+        ssp = np.logical_and(self.SirModel.data[:, Cols.status] == 0, (self.SirModel.data[:, Cols.serviceGuy] == 1))
+
+        # nonservice infected
+        nip = np.logical_and(self.SirModel.data[:, Cols.status] == 11, self.SirModel.data[:, Cols.serviceGuy] == 0)
+
+        # service infected
+        sip = np.logical_and(self.SirModel.data[:, Cols.status] == 11, self.SirModel.data[:, Cols.serviceGuy] == 1)
+
+        # recovered
+        ir = self.SirModel.data[:, Cols.status] == 22
+
+        # dead
+        id = self.SirModel.data[:, Cols.status] == 99
+
+        self.tpAx.cla()
+        self.tpAx.scatter(x[nsp], y[nsp], s=1, color='b', marker='.', label='non service non infected')
+        self.tpAx.scatter(x[ssp], y[ssp], color='b', marker='+')
+        self.tpAx.scatter(x[nip], y[nip], s=4, color='r', marker='.')
+        self.tpAx.scatter(x[sip], y[sip], color='r', marker='+')
+        self.tpAx.scatter(x[ir], y[ir], s=4, color='g', marker='.')
+        self.tpAx.scatter(x[id], y[id], color='k', marker='*')
+
+        self.SirModel.counts[self.day, 0] = np.count_nonzero(nsp) + np.count_nonzero(ssp)  # suspectable
+        self.SirModel.counts[self.day, 1] = np.count_nonzero(nip) + np.count_nonzero(sip)  # infected
+        self.SirModel.counts[self.day, 2] = np.count_nonzero(ir)                     # recovered
+        self.SirModel.counts[self.day, 3] = np.count_nonzero(id)                     # dead
+        self.SirModel.counts[self.day, 4] = self.SirModel.newInfected
+
+        days = np.arange(0, self.day + 1)
+        self.PvDax.set_title("Counts of people vs Day")
+        self.PvDax.set_xlabel('day')
+        self.PvDax.set_ylabel('Count at each status')
+
+        colors = ['b','r','g','k']
+        labels = ['non-infected', 'infected', 'recovered','dead']
+
+        if self.newPlot:
+            self.PvDax.cla()
+            self.ax2.cla()
+
+        self.ax2.plot(days, self.SirModel.counts[days, 4],color='r', marker='.', markersize='4', linewidth='.1')
+        self.ax2.set_ylabel('new infected', color='r')
+        self.ax2.tick_params(axis='y', labelcolor='r')
+
+        for iplt in range(1,4):
+            if self.newPlot:
+                self.PvDax.plot(days, self.SirModel.counts[days, iplt], color=colors[iplt], label=labels[iplt])
+                self.PvDax.legend(loc='best')
+            else:
+                self.PvDax.plot(days, self.SirModel.counts[days, iplt], color=colors[iplt])
+
+        self.newPlot = False
+        self.PvDax.grid(True)
+
+        self.Topcanvas.draw()
+        self.PvDcanvas.draw()
+        cnts = self.SirModel.counts[self.day, :].astype(int)
+
+        stats = str(self.day)  \
+                + '\n' + str(cnts[0]) \
+                + '\n' + str(cnts[1]) \
+                + '\n' + str(cnts[2]) \
+                + '\n' + str(cnts[3]) \
+                + '\n' + str(self.SirModel.newInfected) \
+                + '\n' + str(self.SirModel.nRecoveredOrDead)
+
+        self.StatsData.setText(stats)
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    app.setStyleSheet("QToolTip {\
+     font-size:10pt;\
+     color:white; padding:2px;\
+     border-width:2px;\
+     border-style:solid;\
+     border-radius:20px;\
+     background-color: black;\
+     border: 1px solid white;\
+     overflow:hidden;}");
+
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec_())
+
---
+++ SirModel.py
@@ -0,0 +1,187 @@
+import numpy as np
+import math
+from enum import IntEnum
+from PyQt5 import QtCore, QtWidgets
+
+class Cols(IntEnum):   # cols of the SirModel data array
+    group,\
+    status,\
+    dayInfec,\
+    nStrgrs,\
+    serviceGuy,\
+    xPos,\
+    yPos,\
+    isolated,\
+    nInfected,\
+    lastDataCol = range(10)
+
+class Ctrls(IntEnum):
+    nPeeps,\
+    minDaysSick,\
+    pRecover, pDie,\
+    nOwn,\
+    nStrangers, StgrRng, \
+    pServiceGuy, ServNstgrs, ServStrangerRange, \
+    pInfectOwn, pInfectStgr, \
+    pContactTrc, nContTracePerDay, \
+    pObeySIP, \
+    SIPthres, SIPnStgr, SIPstrgrRng, SIPpInfectStgr, SIPpInfectOwn, SIPpObey, SIPduration, \
+    LastCtrl = range(23)
+
+
+class SirModel:
+    def __init__(self, cntrlsIn):
+        self.cs = []
+        self.sipEndDay = 0
+        self.SipStartDays = 0
+        self.SipEndDays = 0
+        self.infected = 0
+        self.groups = 0
+        self.serviceGuys = 0
+        self.nonServiceGuys = 0
+        self.infectionRate = 0
+        self.counts = np.zeros((1000, 5))
+        self.Controls = cntrlsIn
+        self.SetCntrls()
+        self.newInfected = 0
+        self.nRecoveredOrDead = 0
+        r = self.cs[Ctrls.nPeeps]
+        c = int(Cols.lastDataCol)
+
+        self.data = np.zeros((self.cs[Ctrls.nPeeps], Cols.lastDataCol))
+        self.Reset()
+
+    def SetCntrls(self):
+        self.cs.clear()
+        for i, ctrl in enumerate(self.Controls):
+            line:QtWidgets.QLineEdit = ctrl
+            text = line.text()
+            value = 0
+            try:
+                value = int(text)
+            except:
+                try:
+                    value = float(text)
+                finally:
+                   pass
+
+            self.cs.append(value)
+
+    def InfectedToRecovered(self, day):
+        infected = np.nonzero(self.data[:,Cols.status] == 11)[0]
+        longSickGuys = infected[day - self.data[infected, Cols.dayInfec] > self.cs[Ctrls.minDaysSick]]
+
+        if longSickGuys.size == 0:
+            return
+
+        urand = np.random.sample(len(longSickGuys))
+        died = longSickGuys[self.cs[Ctrls.pDie] > urand]
+        self.data[died, Cols.status] = 99
+        infected = np.nonzero(self.data[:,Cols.status] == 11)
+
+        gonners = np.nonzero((day - self.data[infected,Cols.dayInfec]) > 20)[0]
+        self.data[gonners,Cols.status] = 99
+
+        urand2 = np.random.sample(len(longSickGuys))
+
+        # recovers = longSickGuys(S.cs(Ctrls.pRecover) > randDraw);
+
+        recovers = longSickGuys[self.cs[Ctrls.pRecover] > urand2]
+        self.data[recovers, Cols.status] = 22
+
+        self.nRecoveredOrDead = np.count_nonzero(recovers) + np.count_nonzero(died) + np.count_nonzero(gonners)
+
+    def SpreadInfection(self, day):
+        infected = np.nonzero(self.data[:,Cols.status] == 11)
+        self.newInfected = 0
+        if infected[0].size == 0:
+            return
+
+        for ii in np.nditer(infected):
+            closeAss = np.array(self.groups[int(self.data[ii, Cols.group])])
+            suscepts = closeAss[self.data[closeAss, Cols.status] == 0]
+            urand = np.random.sample(len(suscepts))
+            newSickClose = suscepts[urand < self.cs[Ctrls.pInfectOwn]]
+
+            self.data[newSickClose, Cols.dayInfec] = day
+            self.data[newSickClose, Cols.status] = 11
+
+            #  generate and check strangers
+            farthestStranger = self.cs[Ctrls.StgrRng]
+            nstrangers = self.cs[Ctrls.nStrangers]
+            if self.data[ii, Cols.serviceGuy]:
+                farthestStranger = self.cs[Ctrls.ServStrangerRange]
+                nstrangers = self.cs[Ctrls.ServNstgrs]
+
+            belowStart = max(0, closeAss[0] - farthestStranger)
+            belowEnd = max(0, closeAss[0])
+            aboveStart = min(closeAss[-1] + 1, self.cs[Ctrls.nPeeps] - 1)
+            aboveEnd = min(closeAss[-1] + 1 + farthestStranger, self.cs[Ctrls.nPeeps] - 1)
+            strangers = np.r_[np.arange(belowStart,belowEnd), np.arange(aboveStart,aboveEnd)]
+            index = range(len(strangers))
+            sgIndex = np.random.permutation(index)
+            sgIndex = sgIndex[0:nstrangers]
+            strangers = strangers[sgIndex]
+
+            suscepts2 = strangers[self.data[strangers, Cols.status] == 0]
+            urand2 = np.random.sample(len(suscepts2))
+            newSickStgr = suscepts2[urand2 < self.cs[Ctrls.pInfectStgr]]
+
+            newII = len(newSickClose) + len(newSickStgr)
+            self.newInfected = self.newInfected + newII
+
+            self.data[ii, Cols.nInfected] = self.data[ii, Cols.nInfected] + newII
+            self.data[newSickStgr, Cols.dayInfec] = day
+            self.data[newSickStgr, Cols.status] = 11
+
+    def Reset(self):
+        self.SetCntrls()
+        self.day = 0
+        self.sipEndDay = 999999
+        self.counts = np.zeros((1000, 5))  # 1000 days, 4 type of counts
+        self.data = np.zeros((self.cs[Ctrls.nPeeps], Cols.lastDataCol))
+        nPeeps2 = math.ceil(self.cs[Ctrls.nPeeps] / 2)
+        self.infected = [nPeeps2, nPeeps2 + 10]
+        self.newInfected = 0
+
+        self.data[self.infected, Cols.status] = 11
+        self.data[self.infected, Cols.dayInfec] = 0
+
+        nextPeep = 0
+        ngrp = 0
+        self.groups = []
+
+        while True:
+            npeep = np.random.poisson(self.cs[Ctrls.nOwn]) + 1
+            lastPeep = min(self.cs[Ctrls.nPeeps], nextPeep + npeep)
+            peeps = [n for n in range(nextPeep, lastPeep)]
+            self.groups.append(peeps)
+            self.data[peeps,Cols.group] = ngrp
+
+            ngrp += 1
+            nextPeep += npeep
+            if nextPeep > self.cs[Ctrls.nPeeps]:
+                x = 0
+                break
+
+        everyone = [n for n in range(self.cs[Ctrls.nPeeps])]
+
+        sgIndex = np.random.permutation(everyone)
+        self.serviceGuys = sgIndex[0: math.floor(self.cs[Ctrls.nPeeps] * self.cs[Ctrls.pServiceGuy])]
+
+        nStrangers = np.random.poisson(self.cs[Ctrls.nOwn], len(self.serviceGuys)) + 1 # ServNstgrs = avg number of peeps per day
+        self.data[self.serviceGuys, Cols.nStrgrs] = nStrangers
+        self.data[self.serviceGuys, Cols.serviceGuy] = 1
+
+         # nonServiceGuys guys, the general public
+        self.nonServiceGuys = np.nonzero(self.data[:, Cols.serviceGuy] == 0)
+        nStrangers = np.random.poisson(self.cs[Ctrls.nStrangers], len(self.nonServiceGuys)) + 1 # nStrangers = avg number of peeps per day
+        self.data[self.nonServiceGuys, Cols.nStrgrs] = nStrangers
+
+        width = 11
+        hgt = 3
+        nCols = round((math.sqrt(self.cs[Ctrls.nPeeps] /hgt / width) * width))
+        nRows = math.ceil(self.cs[Ctrls.nPeeps] / nCols)
+
+        self.data[:, Cols.xPos] = np.mod(everyone, nCols)
+        self.data[:, Cols.yPos] = np.floor(np.divide(everyone, nCols))
